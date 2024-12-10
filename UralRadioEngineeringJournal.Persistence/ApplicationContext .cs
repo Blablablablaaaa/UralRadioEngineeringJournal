6fadaf45c8c0e213c5375292a57c06f8d1e31cb1
Блаблаблаблааа <tokareva_todal@mail.ru>
2024-12-10 21:05:29 +0500

    Добавила слой Persistence

---
+++ UralRadioEngineeringJournal.Persistence/ApplicationContext .cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
